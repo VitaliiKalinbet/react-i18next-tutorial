24993f8dd989355f61bb55a14dc5b7dd3e2e8ea1
VitaliiKalinbet <bosssvit@gmail.com>
2021-07-30 11:03:29 +0300

    change i18next init

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom";
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-// import HttpApi from "i18next-http-backend";
 
 import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -45,16 +44,12 @@ const resources = {
 i18next
   .use(initReactI18next)
   .use(LanguageDetector)
-  // .use(HttpApi)
   .init({
     supportedLngs: ["en", "fr", "ar"],
     fallbackLng: "en",
     detection: {
       order: ["path", "cookie", "htmlTag", "localStorage", "subdomain"],
       caches: ["cookie"],
-    },
-    backend: {
-      loadPath: "/assets/locales/{{lng}}/translation.json",
     },
     react: {
       useSuspense: false,
